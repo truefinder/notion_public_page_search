195af6a55950bbe0812edc4e5c6a1d55328d86c9
Seunghyun Seo <truefroggie@gmail.com>
2025-09-08 04:28:46 +0900

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+# Notion Integration Token
+NOTION_TOKEN = "your_notion_api_key"
+
+# Output file names
+DEFAULT_JSON_OUTPUT = "notion_security_scan.json"
+DEFAULT_CSV_OUTPUT = "notion_security_scan.csv"
